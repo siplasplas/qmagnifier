9455faa08e2d992db31c59574fd79b2f571a2124
Andrzej Borucki <borucki.andrzej@gmail.com>
2023-07-30 02:15:55 +0200

    scale = 8; get RGB

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@
 #include <QMainWindow>
 #include <QMenuBar>
 #include <QAction>
+#include <QStatusBar>
+#include <cmath>
+#include <algorithm>
 
 class Magnifier : public QLabel {
 Q_OBJECT
@@ -14,32 +17,62 @@ public:
     explicit Magnifier(QWidget* parent = nullptr)
             : QLabel(parent)
     {
+        setMouseTracking(true);
     }
+    void mouseMoveEvent(QMouseEvent* event) override;
 };
 
 class MainWindow : public QMainWindow {
 Q_OBJECT
     Magnifier *magnifier;
+    float scale = 8;
 protected:
     void mouseMoveEvent(QMouseEvent* event) override;
 public:
     explicit MainWindow(QWidget* parent = nullptr)
             : QMainWindow(parent)
     {
-        magnifier = new Magnifier();
+        magnifier = new Magnifier(this);
         setCentralWidget(magnifier);
-        // Tworzy menu.
         QMenu *fileMenu = menuBar()->addMenu(tr("&File"));
         QAction *exitAction = fileMenu->addAction(tr("E&xit"), this, &QWidget::close);
         exitAction->setShortcuts(QKeySequence::Quit);
+        statusBar()->showMessage(tr("Ready"));
     }
 };
+
+void Magnifier::mouseMoveEvent(QMouseEvent* event) {
+    QPixmap pixmap = grab();
+    QImage image = pixmap.toImage();
+    QColor color = image.pixelColor(event->x(), event->y());
+    int red = color.red();
+    int green = color.green();
+    int blue = color.blue();
+    QMainWindow* mwin = dynamic_cast<QMainWindow*>(parent());
+    QString s = QString("%1, %2, %3 ").arg(red).arg(green).arg(blue);
+    mwin->statusBar()->showMessage(s);
+    QLabel::mouseMoveEvent(event);
+}
 
 void MainWindow::mouseMoveEvent(QMouseEvent* event) {
     QScreen *screen = QApplication::screenAt(event->globalPos());
     if (screen) {
-        const QPixmap screenshot = screen->grabWindow(0, event->globalX() - 100, event->globalY() - 100, 200, 200);  // Dostosuj do swojej lupy.
-        magnifier->setPixmap(screenshot.scaled(magnifier->width(), magnifier->height()));
+        int mw = magnifier->width();
+        int mh = magnifier->height();
+        int scrW = round(mw/scale);
+        int scrH = round(mh/scale);
+        int cx = event->globalX();
+        int cy = event->globalY();
+        int startX = cx-scrW/2;
+        int startY = cy-scrH/2;
+        startX = std::max(startX, 0);
+        startY = std::max(startY, 0);
+        if (startX+scrW > screen->geometry().right())
+            startX = screen->geometry().right() - scrW;
+        if (startY+scrH > screen->geometry().bottom())
+            startY = screen->geometry().bottom() - scrH;
+        const QPixmap screenshot = screen->grabWindow(0, startX, startY, scrW, scrH);
+        magnifier->setPixmap(screenshot.scaled(mw, mh));
         magnifier->show();
     }
 }
